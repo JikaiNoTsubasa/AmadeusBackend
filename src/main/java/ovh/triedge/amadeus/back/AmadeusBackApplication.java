bd51ea452aea004f9d2bfa3f0fad73c785a7dbaf
JikaiNoTsubasa <stephane.biehler.priv@gmail.com>
2024-09-22 10:27:20 +0200

    Initial spring boot app

---
+++ src/main/java/ovh/triedge/amadeus/back/AmadeusBackApplication.java
@@ -0,0 +1,11 @@
+package ovh.triedge.amadeus.back;
+
+import org.springframework.boot.SpringApplication;
+
+public class AmadeusBackApplication {
+
+	// http://localhost:8080/swagger-ui/index.html
+    public static void main(String[] args) {
+        SpringApplication.run(AmadeusBackApplication.class, args);
+    }
+}
